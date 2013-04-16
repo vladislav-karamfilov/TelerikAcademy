e09f73c508ee6107404cc307bec61d4b8e2647fa
Vladislav Karamfilov <v_karamfilov@abv.bg>
2013-04-16 23:32:43 +0300

    High-quality code directory added

---
+++ C# High-quality code/UsingVariablesDataExpressionsConstants/1. RefactorUsingVariables/RectangleRotationDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class RectangleRotationDemo
+{
+    static void Main()
+    {
+        Rectangle rectangleToRotate = new Rectangle(5, 6);
+        double angleToRotate = Math.PI / 3;
+
+        Rectangle rotatedRectangle = GetRotatedRectangle(rectangleToRotate, angleToRotate);
+        Console.WriteLine("The width of the new rectangle is: " + rotatedRectangle.Width);
+        Console.WriteLine("The height of the new rectangle is: " + rotatedRectangle.Height);
+    }
+
+    public static Rectangle GetRotatedRectangle(Rectangle rectangleToRotate, double angleInRadians)
+    {
+        double cosineOfAngleToRotate = Math.Cos(angleInRadians);
+        double absoluteCosineOfAngleToRotate = Math.Abs(cosineOfAngleToRotate);
+        double sineOfAngleToRotate = Math.Sin(angleInRadians);
+        double absoluteSineOfAngleToRotate = Math.Abs(sineOfAngleToRotate);
+        double rectangleToRotateWidth = rectangleToRotate.Width;
+        double rectangleToRotateHeight = rectangleToRotate.Height;
+
+        double rotatedRectangleWidth = absoluteCosineOfAngleToRotate * rectangleToRotateWidth + 
+            absoluteSineOfAngleToRotate * rectangleToRotateHeight;
+        double rotatedRectangleHeight = absoluteSineOfAngleToRotate * rectangleToRotateWidth +
+            absoluteCosineOfAngleToRotate * rectangleToRotateHeight;
+
+        Rectangle rotatedRectangle = new Rectangle(rotatedRectangleWidth, rotatedRectangleHeight);
+        return rotatedRectangle;
+    }
+}
+
+public class Rectangle
+{
+    public double Width { get; private set; }
+    public double Height { get; private set; }
+
+    public Rectangle(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+}
---
+++ C# High-quality code/UsingVariablesDataExpressionsConstants/2. RefactorVariableUsageAndExpressions/StatisticsPrintDemo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace RefactorVariableUsageAndExpressions
+{
+    class StatisticsPrintDemo
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating a random array of numbers
+            double[] numbers = new double[] { -2.2, 12.421, -21.2, 7346.21, 325.6, 222, -121, -2111 };
+            int numbersCount = numbers.Length;
+
+            StatisticsPrinter printer = new StatisticsPrinter();
+            printer.PrintStatistics(numbers, numbersCount);
+        }
+    }
+
+    class StatisticsPrinter
+    {
+        public void PrintStatistics(double[] numbersArray, int numbersCount)
+        {
+            double maxElement = GetMax(numbersArray, numbersCount);
+            PrintMax(maxElement);
+
+            double minElement = GetMin(numbersArray, numbersCount);
+            PrintMin(minElement);
+
+            double average = GetAverage(numbersArray, numbersCount);
+            PrintAverage(average);
+        }
+
+        private double GetMax(double[] numbersArray, int numbersCount)
+        {
+            double max = double.MinValue;
+            for (int index = 0; index < numbersCount; index++)
+            {
+                if (numbersArray[index] > max)
+                {
+                    max = numbersArray[index];
+                }
+            }
+
+            return max;
+        }
+
+        private void PrintMax(double maxNumber)
+        {
+            Console.WriteLine("The maximal number is: " + maxNumber);
+        }
+
+        private double GetMin(double[] numbersArray, int numbersCount)
+        {
+            double min = double.MaxValue;
+            for (int index = 0; index < numbersCount; index++)
+            {
+                if (numbersArray[index] < min)
+                {
+                    min = numbersArray[index];
+                }
+            }
+
+            return min;
+        }
+
+        private void PrintMin(double minNumber)
+        {
+            Console.WriteLine("The minimal number is: " + minNumber);
+        }
+
+        private double GetAverage(double[] numbersArray, int numbersCount)
+        {
+            double sum = 0;
+            for (int index = 0; index < numbersCount; index++)
+            {
+                sum += numbersArray[index];
+            }
+
+            double average = sum / numbersCount;
+            return average;
+        }
+
+        private void PrintAverage(double average)
+        {
+            Console.WriteLine("The average is: " + average);
+        }
+    }
+}
